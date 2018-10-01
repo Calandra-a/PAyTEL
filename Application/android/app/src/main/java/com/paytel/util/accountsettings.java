b41989671bf128e2198b495f197752902560944f
Marsol Daman <mdaman@oakland.edu>
2018-09-30 23:55:57 -0400

    -Design of Settings Page

--- Application/android/app/src/main/java/com/paytel/util/accountsettings.java
+++ Application/android/app/src/main/java/com/paytel/util/accountsettings.java
@@ -3,16 +3,19 @@ package com.paytel.util;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.paytel.util.userData;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBHashKey;
 import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBTable;
 import com.paytel.R;
-
 import java.util.Map;
 import java.util.Set;
 
 @DynamoDBTable(tableName = "csi-mobilehub-447478737-user-data")
 public class accountsettings extends AppCompatActivity {
+    userData current_user;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,20 +24,12 @@ public class accountsettings extends AppCompatActivity {
 
         Button btn_SAVE_userinfo = findViewById(R.id.btn_save_userinfo);
         Button btn_BACK_homepage = findViewById(R.id.btn_back_homepage);
+
+
     }
 
-    private String _userId;
-    private String _city;
-    private String _country;
-    private Map<String, String> _creditCard;
-    private String _eMail;
-    private String _firstName;
-    private String _lastName;
-    private String _phoneNumber;
-    private Set<String> _rekognitionIds;
-    private String _street;
-    private String _username;
-    private String _zipCode;
+
+
 
 
 

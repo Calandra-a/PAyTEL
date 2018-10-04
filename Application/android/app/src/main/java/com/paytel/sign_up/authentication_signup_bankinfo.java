868c7e397acc1f0c072866f7bb850c737aef3d9d
apdapoz <apdapoz@oakland.edu>
2018-10-04 01:37:27 -0400

    made the payment method screen look pretty

--- Application/android/app/src/main/java/com/paytel/sign_up/authentication_signup_bankinfo.java
+++ Application/android/app/src/main/java/com/paytel/sign_up/authentication_signup_bankinfo.java
@@ -2,10 +2,13 @@ package com.paytel.sign_up;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.paytel.R;
 import com.paytel.global_objects;
@@ -48,16 +51,16 @@ public class authentication_signup_bankinfo  extends AppCompatActivity {
     }
 
     void add_creditcard(){
-        TextView name_on_card = findViewById(R.id.txt_name_on_card);
-        TextView card_number = findViewById(R.id.txt_card_number);
-        TextView CVC = findViewById(R.id.txt_cvc);
-        TextView exp_date = findViewById(R.id.txt_exp_date);
+        TextInputLayout name_on_card = findViewById(R.id.txt_name_on_card);
+        TextInputLayout card_number = findViewById(R.id.txt_card_number);
+        TextInputLayout CVC = findViewById(R.id.txt_cvc);
+        TextInputLayout exp_date = findViewById(R.id.txt_exp_date);
 
         Map<String, String> cc = new HashMap<String, String>();
-        cc.put("name_on_card",name_on_card.getText().toString());
-        cc.put("card_number", card_number.getText().toString());
-        cc.put("cvc", CVC.getText().toString());
-        cc.put("expiration_date", exp_date.getText().toString());
+        cc.put("name_on_card",name_on_card.getEditText().getText().toString());
+        cc.put("card_number", card_number.getEditText().getText().toString());
+        cc.put("cvc", CVC.getEditText().getText().toString());
+        cc.put("expiration_date", exp_date.getEditText().getText().toString());
 
         new_user.setCreditCard(cc);
     }

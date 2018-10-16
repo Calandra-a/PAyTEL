54556481aee3694109939cd3f0e889d7caf0715c
Axel Van Hoyweghen <Axel.van.hoyweghen@outlook.com>
2018-10-16 12:18:44 -0400

    added api interface to master

---
+++ Application/android/app/src/main/java/com/paytel/util/api/id4pxy6j7c4b/AuthenticatefacialMobileHubClient.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2010-2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://aws.amazon.com/apache2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.paytel.util.api.id4pxy6j7c4b;
+import java.util.*;
+
+
+
+@com.amazonaws.mobileconnectors.apigateway.annotation.Service(endpoint = "https://4pxy6j7c4b.execute-api.us-east-1.amazonaws.com/Development")
+
+public interface AuthenticatefacialMobileHubClient {
+
+
+    /**
+     * A generic invoker to invoke any API Gateway endpoint.
+     * @param request
+     * @return ApiResponse
+     */
+    com.amazonaws.mobileconnectors.apigateway.ApiResponse execute(com.amazonaws.mobileconnectors.apigateway.ApiRequest request);
+}
+

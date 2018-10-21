30fcd035a2823189488ffb64f674960a0e4cef95
Anthony <acalandra@oakland.edu>
2018-10-20 21:42:13 -0400

    updated sign up checks

--- Application/android/app/src/main/java/com/paytel/sign_up/authentication_signup_bankinfo.java
+++ Application/android/app/src/main/java/com/paytel/sign_up/authentication_signup_bankinfo.java
@@ -71,8 +71,8 @@ public class authentication_signup_bankinfo  extends AppCompatActivity {
             toast.show();
             return false;
         }
-        else if (card_number.getEditText().getText().toString().length() > 20){
-            CharSequence fail = "Card number can't be over 20 digits";
+        else if (card_number.getEditText().getText().toString().length() != 16){ 
+            CharSequence fail = "Card number must be 16 digits"; 
             Toast toast = Toast.makeText(context, fail, dLong);
             toast.show();
             return false;

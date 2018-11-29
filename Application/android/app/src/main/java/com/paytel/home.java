eab00d728e72efa68d5174ab2f59f2cc26e3e018
Marsol Daman <Marsol Daman>
2018-11-28 22:35:54 -0500

    -Fingerprint added for newer phones -button timeout added

--- Application/android/app/src/main/java/com/paytel/home.java
+++ Application/android/app/src/main/java/com/paytel/home.java
@@ -3,6 +3,7 @@ package com.paytel;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
@@ -68,6 +69,7 @@ public class home extends AppCompatActivity{
     ArrayList<TransactionCard> pendingTransaction = new ArrayList<>();
     Map<String, String> map = new HashMap<String, String>();
     private TransactionAdapter tPendingAdapter, tCompleteAdapter;
+    private long mLastClickTime = 0;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -76,11 +78,19 @@ public class home extends AppCompatActivity{
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
+                    if (SystemClock.elapsedRealtime() - mLastClickTime < 1000){
+                        return true;
+                    }
+                    mLastClickTime = SystemClock.elapsedRealtime();
                     refreshTransactions();
                     nav_bool = false;
                     queryUser();
                     return true;
                 case R.id.navigation_dashboard:
+                    if (SystemClock.elapsedRealtime() - mLastClickTime < 1000){
+                        return true;
+                    }
+                    mLastClickTime = SystemClock.elapsedRealtime();
                     refreshTransactions();
                     nav_bool = true;
                     queryUser();
@@ -117,40 +127,29 @@ public class home extends AppCompatActivity{
         pendinglistView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             public void onItemClick(AdapterView<?> arg0, View arg1,int arg2, long arg3) {
-                /*TextView label = arg1.findViewById(R.id.label);
-                String viewString = label.getText().toString();
-                String transactionNumber = viewString.substring(4,8);
-                String amount = viewString.substring(viewString.lastIndexOf("$") + 1);;*/
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 2000){
+                    return;
+                }
+                mLastClickTime = SystemClock.elapsedRealtime();
                 TextView invis = arg1.findViewById(R.id.txt_invisID);
                 String viewString = invis.getText().toString();
                 Intent intent = new Intent(home.this, start_buyer_transaction.class);
                 intent.putExtra("name", viewString);
                 startActivity(intent);
-                /*if(map.get(transactionNumber) != null){
-                    Intent intent = new Intent(home.this, start_buyer_transaction.class);
-                    intent.putExtra("name", map.get(transactionNumber));
-                    startActivity(intent);
-                }*/
             }
         });
         completedlistView.setOnItemClickListener(new AdapterView.OnItemClickListener()
         {
             public void onItemClick(AdapterView<?> arg0, View arg1,int arg2, long arg3) {
+                if (SystemClock.elapsedRealtime() - mLastClickTime < 2000){
+                    return;
+                }
+                mLastClickTime = SystemClock.elapsedRealtime();
                 TextView invis = arg1.findViewById(R.id.txt_invisID);
                 String viewString = invis.getText().toString();
                 Intent intent = new Intent(home.this, start_buyer_transaction.class);
                 intent.putExtra("name", viewString);
                 startActivity(intent);
-                /*TextView label = arg1.findViewById(R.id.label);
-                String viewString = label.getText().toString();
-                String transactionNumber = viewString.substring(4,8);
-                String amount = viewString.substring(viewString.lastIndexOf("$") + 1);;
-
-                if(map.get(transactionNumber) != null){
-                    Intent intent = new Intent(home.this, start_buyer_transaction.class);
-                    intent.putExtra("name", map.get(transactionNumber));
-                    startActivity(intent);
-                }*/
             }
         });
 
@@ -321,14 +320,13 @@ public class home extends AppCompatActivity{
                     }
                         tCompleteAdapter = new TransactionAdapter(getApplicationContext(), completedTransaction);
                         tPendingAdapter = new TransactionAdapter(getApplicationContext(), pendingTransaction);
-                    //ArrayAdapter adapterCompleted = new ArrayAdapter<>(getApplicationContext(), R.layout.activity_listview, R.id.label, completedTransaction);
-                    //ArrayAdapter adapterPending = new ArrayAdapter<>(getApplicationContext(), R.layout.activity_listview, R.id.label, pendingTransaction);
+
 
                     if(nav_bool == true) {
                         pendinglistView.setVisibility(View.INVISIBLE);
                         completedlistView.setVisibility(View.VISIBLE);
                         completedlistView.setAdapter(tCompleteAdapter);
-                        //completedlistView.setAdapter(adapterCompleted);
+
                         mConstraintSet.clone(mConstraintLayout);
                         mConstraintSet.connect(R.id.completed_list, ConstraintSet.TOP,
                                 R.id.cardView, ConstraintSet.BOTTOM);
@@ -340,7 +338,7 @@ public class home extends AppCompatActivity{
                         completedlistView.setVisibility(View.INVISIBLE);
                         pendinglistView.setVisibility(View.VISIBLE);
                         pendinglistView.setAdapter(tPendingAdapter);
-                        //pendinglistView.setAdapter(adapterPending);
+
                         mConstraintSet.clone(mConstraintLayout);
                         mConstraintSet.connect(R.id.pending_list, ConstraintSet.TOP,
                                 R.id.cardView, ConstraintSet.BOTTOM);

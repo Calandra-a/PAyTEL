09c53c8151bc8f9b276a7028b374089b758de6c9
Anthony <acalandra@oakland.edu>
2018-11-30 22:12:58 -0500

    dialog added

---
+++ Application/android/app/src/main/java/com/paytel/transaction/dialog.java
@@ -0,0 +1,28 @@
+package com.paytel.transaction;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+
+import com.paytel.home;
+
+public class dialog extends AppCompatDialogFragment  {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Information")
+                .setMessage("This is a Dialog")
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        return builder.create();
+    }
+}
